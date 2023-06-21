ae8ecad2b83c9fdf2212bf99f1d0c9a38af22893
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-06-21 15:03:05 -0300

    Condicionar o botão enviar a seleção da checkbox de fotos

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const btnEnviar = document.getElementById('submit-btn');
 const btnLimpar = document.getElementById('clear-btn');
+const checkboxFotos = document.getElementById('agreement');
 
 function submit(event) {
   event.preventDefault();
 }
 
-btnEnviar.addEventListener('click', submit)
+function changeCheckboxFotos(event) {
+  if (event.target.checked === true) {
+    btnEnviar.disabled = false;
+  } else {
+    btnEnviar.disabled = true;
+  }
+}
+
+btnEnviar.addEventListener('click', submit);
+checkboxFotos.addEventListener('change', changeCheckboxFotos);
